3cf8f83d5597a1223bf493cb485cf94b2af07800
Glauber Brennon <glauberbrennon@gmail.com>
2025-12-01 22:47:39 -0300

    Refactor FizzBuzz into domain layer with enum

--- src/fizzbuzz.py
+++ src/fizzbuzz.py
@@ -1,13 +1,4 @@
-def fizzbuzz(n: int) -> str:
-    """Return FizzBuzz solution for n."""
-    if n % 15 == 0:
-        return "FizzBuzz"
-    elif n % 3 == 0:
-        return "Fizz"
-    elif n % 5 == 0:
-        return "Buzz"
-    else:
-        return str(n)
+from domain import fizzbuzz
 
 
 if __name__ == "__main__":
